4a099eabf1a23113a2dd289024b0bd2cbcc11fd7
KASRO1 <nikita150489@mail.ru>
2024-12-15 15:18:44 +0500

    ss

--- resources/views/init.blade.php
+++ resources/views/init.blade.php
@@ -1,11 +1,12 @@
+<script src="https://telegram.org/js/telegram-web-app.js?56"></script>
+
 <script>
-const tg = window.Telegram.WebApp;
-tg.ready();
-const userinfo = tg.initDataUnsafe.user;
-if (userinfo) {
-    window.location.href = '/generate/token/' + userinfo.id;
-
-
-
-}
+    const tg = window.Telegram.WebApp;
+    tg.ready();
+    const userinfo = tg.initDataUnsafe.user;
+    if (userinfo) {
+        window.location.href = '/generate/token/' + userinfo.id;
+    } else {
+        window.Telegram.WebApp.close();
+    }
 </script>
