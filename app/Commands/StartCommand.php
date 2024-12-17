f185b126402cf7a9814d8d308b0f15dc41a83161
KASRO1 <nikita150489@mail.ru>
2024-12-17 22:39:11 +0500

    ss

--- app/Commands/StartCommand.php
+++ app/Commands/StartCommand.php
@@ -17,64 +17,75 @@ class StartCommand extends Command
 
     public function handle()
     {
-        $message = $this->getUpdate()->getMessage()->text;
-        $setting = Setting::first();
-        $ref = null;
-        if (preg_match('/^\/start (\d+)$/', $message, $matches)) {
-            $user = User::where('tg_id', $matches[1])->first();
-            if ($user) {
-                $user->coins += $setting->bonus_start;
-                $user->save();
-                $ref = $matches[1];
-            } 
-        }
-        $auth_token = bin2hex(random_bytes(16));
-        $user = User::where('tg_id', $this->getUpdate()->getMessage()->from->id)->first();
-        if (!$user) {
-            $user = User::create([
-                'tg_id' => $this->getUpdate()->getMessage()->from->id,
-                'username' => $this->getUpdate()->getMessage()->from->username,
-                'auth_token' => $auth_token,
-            ]);
-            if ($ref) {
-                $stat = StatLink::where('id', $ref)->first();
-                if ($stat) {
-                    $stat->count_start++;
-                    $stat->save();
-                }
-                $user->coins += $setting->bonus_start;
-                $user->save();
+        try {
+            $message = $this->getUpdate()->getMessage()->text;
+            $setting = Setting::first();
+            $ref = null;
+            if (preg_match('/^\/start (\d+)$/', $message, $matches)) {
+                $user = User::where('tg_id', $matches[1])->first();
+                if ($user) {
+                    $user->coins += $setting->bonus_start;
+                    $user->save();
+                    $ref = $matches[1];
+                } 
+            }
+            $auth_token = bin2hex(random_bytes(16));
+            $user = User::where('tg_id', $this->getUpdate()->getMessage()->from->id)->first();
+            if (!$user) {
+                $user = User::create([
+                    'tg_id' => $this->getUpdate()->getMessage()->from->id,
+                    'username' => $this->getUpdate()->getMessage()->from->username,
+                    'auth_token' => $auth_token,
+                ]);
+                if ($ref) {
+                    $stat = StatLink::where('id', $ref)->first();
+                    if ($stat) {
+                        $stat->count_start++;
+                        $stat->save();
+                    }
+                    $user->coins += $setting->bonus_start;
+                    $user->save();
 
-                $user->referral_code = $ref === $user->tg_id ? null : $ref;
+                    $user->referral_code = $ref === $user->tg_id ? null : $ref;
+                    $user->save();
+                }
+            }
+            if (!$user->auth_token) {
+                $user->auth_token = $auth_token;
                 $user->save();
             }
-        }
-        if (!$user->auth_token) {
-            $user->auth_token = $auth_token;
-            $user->save();
-        }
-        $keyboard = [
-            'inline_keyboard' => [
-                [
+            $keyboard = [
+                'inline_keyboard' => [
                     [
-                        'text' => 'Начни играть',
-                        'web_app' => [
-                            'url' => env('APP_URL') . '/login/' . $user->auth_token
+                        [
+                            'text' => 'Играть',
+                            'web_app' => [
+                                'url' => env('APP_URL') . '/login/' . $user->auth_token
+                            ]
                         ]
                     ]
                 ]
-            ]
-        ];
-        $this->replyWithMessage([
-            'text' => "<b>🎉 Добро пожаловать! 🎉</b>
+            ];
+            $this->replyWithMessage([
+                'text' => "<b>🎉 Добро пожаловать! 🎉</b>
 
 Это первый бот в телеграм по игре Brawl Stars, позволяющий тебе получить донат за клики!
 
 Прокачивай бусты, зови друзей, копи монеты Brawl Coin и забирай призы уже сегодня!
 
 <b>👇 Нажимай на кнопку играть и начинай тапать прямо сейчас!</b>",
-            'reply_markup' => json_encode($keyboard),
-            'parse_mode' => 'HTML'
-        ]);
+                'reply_markup' => json_encode($keyboard),
+                'parse_mode' => 'HTML'
+            ]);
+            
+        } catch (\Telegram\Bot\Exceptions\TelegramResponseException $e) {
+            if ($e->getCode() == 403) {
+                // Пользователь заблокировал бота - логируем это
+                Log::info('User blocked bot: ' . $this->getUpdate()->getMessage()->from->id);
+                return;
+            }
+            // Другие ошибки пробрасываем дальше
+            throw $e;
+        }
     }
 }
