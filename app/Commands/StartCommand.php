88d8c982284fa15d78cc4ea33107f25d922f3aaa
KASRO1 <nikita150489@mail.ru>
2024-12-23 05:04:35 +0500

    ss

--- app/Commands/StartCommand.php
+++ app/Commands/StartCommand.php
@@ -42,6 +42,7 @@ class StartCommand extends Command
 
         if (!$user->exists) {
             $user->fill([
+                'tg_id' => $fromId,
                 'username' => $this->getUpdate()->getMessage()->from->username,
                 'auth_token' => bin2hex(random_bytes(16)),
                 'referral_code' => $ref === $fromId ? null : $ref
